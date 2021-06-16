1b07c5baaf279850a374161d3b7b2ab0fb5f9206
Prateek Varma <prtk.varma@gmail.com>
2021-06-16 14:27:58 -0600

    created route-based folders in pages, and created about page (index)

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about-page">
+        <h1>About Page</h1>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+        <p>Lorem Ipsum</p>
+    </div>
+</template>
+
+<style scoped>
+    .about-page {
+        text-align: center;
+        padding: 30px;
+    }
+</style>
