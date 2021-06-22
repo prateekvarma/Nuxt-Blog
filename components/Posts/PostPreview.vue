493cae19057994591ca08f011c34f78a74090db2
Prateek Varma <prtk.varma@gmail.com>
2021-06-22 16:48:01 -0600

    created separate post component and attached to index page

---
+++ components/Posts/PostPreview.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <nuxt-link :to="'/posts/' + id">
+          <article>
+            <div>
+              <h1>{{ title }}</h1>
+              <p>{{ previewText }}</p>
+              <div :style="{backgroundImage: 'url(' + thumbnail + ')' }"></div>
+            </div>
+          </article>
+        </nuxt-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PostPreview',
+    props: {
+        id: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        previewText: {
+            type: String,
+            required: true
+        },
+        thumbnail: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,29 +5,30 @@
         Nuxt Blog
       </h1>
       <div class="links">
-        <nuxt-link :to="'/posts/' + 1">
-          <article>
-            <div>
-              <h1>Post 1 title</h1>
-              <p>Post 1 text</p>
-            </div>
-          </article>
-        </nuxt-link>
-        <nuxt-link :to="'/posts/' + 2">
-          <article>
-            <div>
-              <h1>Post 2 title</h1>
-              <p>Post 2 text</p>
-            </div>
-          </article>
-        </nuxt-link>
+        <post-preview 
+          id="1"
+          thumbnail="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+          title="First title"
+          previewText="First Preview Text"
+        />
+        <post-preview 
+          id="2"
+          thumbnail="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"
+          title="Second title"
+          previewText="Second Preview Text"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import PostPreview from '~/components/Posts/PostPreview.vue'
+export default {
+  components: {
+    PostPreview
+  }
+}
 </script>
 
 <style>
