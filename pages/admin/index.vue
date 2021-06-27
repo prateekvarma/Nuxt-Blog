b323ff23bb818ea0288138f922a9bc8adfb97094
Prateek Varma <prtk.varma@gmail.com>
2021-06-26 22:03:16 -0600

    added pages for admin section

---
+++ pages/admin/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <section>
+            <button>Create Post</button>
+        </section>
+        <section>
+            <h1>Existing Posts</h1>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
