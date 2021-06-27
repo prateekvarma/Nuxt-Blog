cefc65d273ee910c5a27ba64f97e6f7fbbe9885e
Prateek Varma <prtk.varma@gmail.com>
2021-06-26 22:26:14 -0600

    added post list on admin index page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -5,12 +5,17 @@
         </section>
         <section>
             <h1>Existing Posts</h1>
+            <post-list />
         </section>
     </div>
 </template>
 
 <script>
+import PostList from '~/components/Posts/PostList.vue'
+
 export default {
-    
+    components: {
+        PostList
+    }
 }
 </script>
