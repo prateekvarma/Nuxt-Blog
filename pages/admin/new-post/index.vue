380d033f91d8bec068d59ca62b590ea3269673db
Prateek Varma <prtk.varma@gmail.com>
2021-07-04 14:17:34 -0600

    created structure for new-post index page

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,7 +1,44 @@
 <template>
     <div>
         <section>
-            
+            <form @submit.prevent="onSave">
+                <app-control-input v-model="editedPost.author">Author Name</app-control-input>
+                <app-control-input v-model="editedPost.title">Title</app-control-input>
+                <app-control-input v-model="editedPost.thumbnailLink">Thumbnail Link</app-control-input>
+                <app-control-input controlType="textarea" v-model="editedPost.content">Content</app-control-input>
+                <app-button type="submit">Save</app-button>
+                <app-button type="buttom" @click="onCancel">Cancel</app-button>
+            </form>
         </section>
     </div>
 </template>
+
+<script>
+import AppControlInput from '~/components/UI/AppControlInput.vue'
+import AppButton from '~/components/UI/AppButton.vue'
+export default {
+    data() {
+        return {
+            editedPost: {
+                author: '',
+                title: '',
+                thumbnailLink: '',
+                content: ''
+            }
+        }
+    },
+    components: {
+        AppControlInput,
+        AppButton
+    },
+    methods: {
+        onSave() {
+
+        },
+        onCancel() {
+            
+        }
+    }
+    
+}
+</script>
