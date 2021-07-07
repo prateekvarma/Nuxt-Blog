3d350a7e4ab4f3f55be2bc374b15cd5afaed42d6
Prateek Varma <prtk.varma@gmail.com>
2021-07-06 19:15:54 -0600

    created separate component for Admin form

---
+++ components/Admin/AdminPostForm.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <form @submit.prevent="onSave">
+                <app-control-input v-model="editedPost.author">Author Name</app-control-input>
+                <app-control-input v-model="editedPost.title">Title</app-control-input>
+                <app-control-input v-model="editedPost.thumbnailLink">Thumbnail Link</app-control-input>
+                <app-control-input controlType="textarea" v-model="editedPost.content">Content</app-control-input>
+                <app-button type="submit">Save</app-button>
+                <app-button type="buttom" @click="onCancel">Cancel</app-button>
+            </form>
+    </div>
+</template>
+
+<script>
+import AppControlInput from '~/components/UI/AppControlInput.vue'
+import AppButton from '~/components/UI/AppButton.vue'
+export default {
+    data() {
+        return {
+            editedPost: {
+                author: '',
+                title: '',
+                thumbnailLink: '',
+                content: ''
+            }
+        }
+    },
+    components: {
+        AppControlInput,
+        AppButton
+    },
+    methods: {
+        onSave() {
+            console.log(this.editedPost);
+        },
+        onCancel() {
+            // navigate back
+            // to access router, we need "this" as it's required to access injected properties
+            this.$router.push('/admin');
+        }
+    }
+    
+}
+</script>
--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,46 +1,17 @@
 <template>
     <div>
         <section>
-            <form @submit.prevent="onSave">
-                <app-control-input v-model="editedPost.author">Author Name</app-control-input>
-                <app-control-input v-model="editedPost.title">Title</app-control-input>
-                <app-control-input v-model="editedPost.thumbnailLink">Thumbnail Link</app-control-input>
-                <app-control-input controlType="textarea" v-model="editedPost.content">Content</app-control-input>
-                <app-button type="submit">Save</app-button>
-                <app-button type="buttom" @click="onCancel">Cancel</app-button>
-            </form>
+            <admin-post-form />
         </section>
     </div>
 </template>
 
 <script>
-import AppControlInput from '~/components/UI/AppControlInput.vue'
-import AppButton from '~/components/UI/AppButton.vue'
+import AdminPostForm from '~/components/Admin/AdminPostForm.vue'
+
 export default {
-    data() {
-        return {
-            editedPost: {
-                author: '',
-                title: '',
-                thumbnailLink: '',
-                content: ''
-            }
-        }
-    },
     components: {
-        AppControlInput,
-        AppButton
-    },
-    methods: {
-        onSave() {
-            console.log(this.editedPost);
-        },
-        onCancel() {
-            // navigate back
-            // to access router, we need "this" as it's required to access injected properties
-            this.$router.push('/admin');
-        }
+        AdminPostForm
     }
-    
 }
 </script>
