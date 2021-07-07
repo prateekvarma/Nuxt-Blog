a16dd9b3e082e7e97b12b691869da15cb4dc35bd
Prateek Varma <prtk.varma@gmail.com>
2021-07-06 19:40:43 -0600

    created admin page to show post

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <admin-post-form />
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '~/components/Admin/AdminPostForm.vue'
+export default {
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Prateek',
+                title: 'The title',
+                content: 'This is the content',
+                thumbnailLink: 'https://picture.jpeg'
+            }
+        }
+    }
+}
+</script>
